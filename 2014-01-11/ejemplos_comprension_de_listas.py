5655fba61ee6e4ac44b833e63fe146f1f2f35a02
Antonio Ognio <antonio@ognio.com>
2016-02-20 11:05:20 -0500

    Update ejemplos_comprension_de_listas.py

--- 2014-01-11/ejemplos_comprension_de_listas.py
+++ 2014-01-11/ejemplos_comprension_de_listas.py
@@ -6,3 +6,13 @@
 print [x**2 for x in range(5) if x % 2 == 0]
 
 print [p.upper() for p in [u'Pistola', u'Pelota', u'Lápiz'] if len(p) % 2 == 1]
+
+# Sin utilizar listas por comprensión
+
+por_imprimir = []
+
+for p in [u'Pistola', u'Pelota', u'Lápiz']:
+    if len(p) % 2 == 1:
+        por_imprimir.append(p.upper())
+
+print por_imprimir
